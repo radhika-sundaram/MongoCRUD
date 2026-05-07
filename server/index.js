419dd5cb14b05426c5e03c84e912e532a1c30a43
radhika-sundaram <radhikasundaram1@gmail.com>
2026-05-07 14:33:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000',
-    'https:'//mongo-crud.vercel.app/',
+    'https://mongo-crud.vercel.app'
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }   
 ));
